a91f6360b143845c8bf310bc54f572562c9d3b8f
CharlesL-Sec <173039445+CharlesL-Sec@users.noreply.github.com>
2024-10-31 15:10:30 +0000

    Update servicenow-installer.js
    Speciify windows and architecture.

--- servicenow-installer.js
+++ servicenow-installer.js
@@ -1,4 +1,4 @@
 var buildstamp = gs.getProperty('mid.buildstamp')
-var m = new MIDPackage('mid', buildstamp, 'linux', 'x86-64');
+var m = new MIDPackage('mid', buildstamp, 'windows', 'x86-64');
 var uri = new MIDPackageUriFactory(m, 'attachment_download').getUris(1)[0];
 gs.print(uri);
