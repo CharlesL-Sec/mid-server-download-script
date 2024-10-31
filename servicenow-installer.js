09b264a71af84ab935a871691964a140ed18956b
Charles <charles@new-web-sites.com>
2024-10-31 15:05:13 +0000

    Create servicenow-installer.js

---
+++ servicenow-installer.js
@@ -0,0 +1,4 @@
+var buildstamp = gs.getProperty('mid.buildstamp')
+var m = new MIDPackage('mid', buildstamp, 'linux', 'x86-64');
+var uri = new MIDPackageUriFactory(m, 'attachment_download').getUris(1)[0];
+gs.print(uri);
